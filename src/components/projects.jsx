9a39440b8907b91913765682e3bc217799020646
Elizabeth Einstein <eeinstein134@gmail.com>
2020-04-26 16:58:37 -0600

    Made projects section responsive

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -30,18 +30,16 @@ const Projects = () => {
                         <div className="projects-grid">
                             <div className="lalalime">
                                 <Img fluid={data.lalalime.childImageSharp.fluid} />
-                                <p>An e-commerce site that sells women's leggings.</p>
                                 <p>
-                                    Stack: JavaScript, Node, Express, Sequelize, PostgreSQL, React, Redux, Stripe API,
-                                    Heroku, Travis CI
+                                    An e-commerce site that sells women's leggings. Built using JavaScript, Node,
+                                    Express, Sequelize, PostgreSQL, React, Redux, Stripe API, Heroku, Travis CI
                                 </p>
                             </div>
                             <div className="hive">
                                 <Img fluid={data.hive.childImageSharp.fluid} />
-                                <p>A collaborative whiteboard with integrated chat.</p>
                                 <p>
-                                    Stack: TypeScript, Node, Express, TypeORM, PostgreSQL, React, Redux, Fabric.js,
-                                    Socket.io, Heroku
+                                    A collaborative whiteboard with integrated chat. Built using TypeScript, Node,
+                                    Express, TypeORM, PostgreSQL, React, Redux, Fabric.js, Socket.io, Heroku
                                 </p>
                             </div>
                         </div>
