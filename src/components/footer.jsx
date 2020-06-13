ce4d8ed644ac653eb2d740e9946e9de5b59bdb8f
Elizabeth Einstein <eeinstein134@gmail.com>
2020-06-12 20:48:22 -0600

    Rewrote image query to load images

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     }
                 }
             }
-            github: file(relativePath: { eq: "technologies/devops/github.png" }) {
+            github: file(relativePath: { eq: "tech/devops/03-github.png" }) {
                 childImageSharp {
                     fluid(maxWidth: 300) {
                         ...GatsbyImageSharpFluid
--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -1,41 +1,110 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import { Img } from "gatsby-image"
 
 const Tech = () => {
     const data = useStaticQuery(graphql`
-        query MyQuery {
-            allFile(filter: {extension: {regex: "/(png)/"}, relativeDirectory: {eq: "technologies"}}) {
-                edges {
-                    node {
-                        relativePath
-                        base
-                        childImageSharp {
-                            fluid {
-                                aspectRatio
-                                base64
-                                sizes
-                                src
-                                srcSet
-                            }
-                        }
+    {
+      languages: allFile(filter: {relativePath: {regex: "/(languages)/"}}) {
+        edges {
+            node {
+                name
+                relativePath
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
                     }
                 }
             }
         }
+      }
+      libraries: allFile(filter: {relativePath: {regex: "/(libraries)/"}}) {
+        edges {
+            node {
+                name
+                relativePath
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+      }
+      db: allFile(filter: {relativePath: {regex: "/(db)/"}}) {
+        edges {
+            node {
+                name
+                relativePath
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+      }
+      devops: allFile(filter: {relativePath: {regex: "/(devops)/"}}) {
+        edges {
+            node {
+                name
+                relativePath
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+      }
+    }
+  `);
 
-    `);
+    const languages = data.languages.edges;
+    const libraries = data.libraries.edges;
+    const db = data.db.edges;
+    const devops = data.devops.edges;
+
+    console.log("Lang", languages, "Lib", libraries, "db", db, "dev", devops)
+
+
     return (
         <div className="technologies">
             <div className="tech-container">
                 <div className="row">
-                    <div className="main-text">Elizabeth Einstein</div>
-                    <div className="main-image">
-                        {data.allFile.edges.map(({ node }) => (
-                            <Img fluid={node.childImageSharp.fluid} />
-                        ))}
+                    <div>
+                        <h4>Languages</h4>
+                        <div className="tech-list">
+                            {languages.map(node =>
+                                <Img fluid={node.childImageSharp.fluid} />
+                            )}
+                        </div>
                     </div>
-                </div>
+                    <div>
+                        <h4>Libraries and Frameworks</h4>
+                        <div className="tech-list">
+                            {libraries.map(node =>
+                                <Img fluid={node.childImageSharp.fluid} />
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        <h4>Databases</h4>
+                        <div className="tech-list">
+                            {db.map(node =>
+                                <Img fluid={node.childImageSharp.fluid} />
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        <h4>DevOps</h4>
+                        <div className="tech-list">
+                            {devops.map(node =>
+                                <Img fluid={node.childImageSharp.fluid} />
+                            )}
+                        </div>
+                    </div>
+                </div >
             </div>
         </div>
     );
