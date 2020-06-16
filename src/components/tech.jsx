4e93690522f07b28cdab530ca86ced6176cee75d
Elizabeth Einstein <eeinstein134@gmail.com>
2020-06-15 22:17:05 -0600

    tech logos now a horizontal grid

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -29,6 +29,8 @@ const Tech = () => {
     const db = edges.filter((edge) => (/db/).test(edge.node.relativePath));
     const devops = edges.filter((edge) => (/devops/).test(edge.node.relativePath));
 
+    console.log(languages)
+
 
     return (
         <div className="tech">
@@ -37,46 +39,44 @@ const Tech = () => {
                     <h3>Technologies</h3>
                 </div>
                 <div className="inner-text">
-                    <div className="tech-section">
-                        <div className="section-head">
-                            <h4>Languages</h4>
-                            <div className="tech-list">
-                                {languages.map((edge, id) =>
-                                    <div className="logo-container">
-                                        <Img key={id} fluid={edge.node.childImageSharp.fluid} />
-                                    </div>
-                                )}
-                            </div>
+                    <div className="section-head">
+                        <h4>Languages</h4>
+                        <div className="tech-list">
+                            {languages.map((edge) =>
+                                <div className="logo-container">
+                                    <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+                                </div>
+                            )}
                         </div>
-                        <div className="section-head">
-                            <h4>Libraries and Frameworks</h4>
-                            <div className="tech-list">
-                                {libraries.map((edge, id) =>
-                                    <div className="logo-container">
-                                        <Img key={id} fluid={edge.node.childImageSharp.fluid} />
-                                    </div>
-                                )}
-                            </div>
+                    </div>
+                    <div className="section-head">
+                        <h4>Libraries and Frameworks</h4>
+                        <div className="tech-list">
+                            {libraries.map((edge) =>
+                                <div className="logo-container">
+                                    <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+                                </div>
+                            )}
                         </div>
-                        <div className="section-head">
-                            <h4>Databases</h4>
-                            <div className="tech-list">
-                                {db.map((edge, id) =>
-                                    <div className="logo-container">
-                                        <Img key={id} fluid={edge.node.childImageSharp.fluid} />
-                                    </div>
-                                )}
-                            </div>
+                    </div>
+                    <div className="section-head">
+                        <h4>Databases</h4>
+                        <div className="tech-list">
+                            {db.map((edge) =>
+                                <div className="logo-container">
+                                    <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+                                </div>
+                            )}
                         </div>
-                        <div className="section-head">
-                            <h4>DevOps</h4>
-                            <div className="tech-list">
-                                {devops.map((edge, id) =>
-                                    <div className="logo-container">
-                                        <Img key={id} fluid={edge.node.childImageSharp.fluid} />
-                                    </div>
-                                )}
-                            </div>
+                    </div>
+                    <div className="section-head">
+                        <h4>DevOps</h4>
+                        <div className="tech-list">
+                            {devops.map((edge) =>
+                                <div className="logo-container">
+                                    <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
